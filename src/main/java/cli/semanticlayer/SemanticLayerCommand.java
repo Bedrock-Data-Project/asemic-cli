cdb4115662178a3992db2ed735d6f7dae5dcdc2d
ikorhner <ikorhner@godaddy.com>
2024-05-03 18:40:59 +0200

    first commig

---
+++ src/main/java/cli/semanticlayer/SemanticLayerCommand.java
@@ -0,0 +1,12 @@
+package cli.semanticlayer;
+
+import picocli.CommandLine;
+
+@CommandLine.Command(name = "semantic-layer", mixinStandardHelpOptions = true, subcommands = {
+    AuthCommand.class,
+    ValidateCommand.class,
+    PushCommand.class,
+    PullCommand.class
+})
+public class SemanticLayerCommand {
+}
